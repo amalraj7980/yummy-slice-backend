0943e9abee90e1492cd1616af95d2517dc7d42b6
AbhiKrishnan <83958925+Abhi-akhila@users.noreply.github.com>
2022-03-08 23:31:50 +0530

    otpVerify and passwordChange are removed

--- controllers/forgotPassword.js
+++ controllers/forgotPassword.js
@@ -34,6 +34,7 @@ exports.forgotPassword = async (req, res) => {
             })
         }
     } catch (err) {
+        console.log("Error occured in forgot password try catch block",err)
         return res.json({
             status: 'FAILED',
             err
@@ -41,60 +42,3 @@ exports.forgotPassword = async (req, res) => {
     }
 }
 
-exports.OTPVerify = async (req, res) => {
-    const {
-        OTP,
-        uuid
-    } = req.body;
-    try {
-        if (OTP !== '' || uuid !== '') {
-            const user = await User.findOne({ where: { uuid, OTP } })
-            if (user) {
-                res.json({
-                    status: 'SUCCESS',
-                    message: 'OTP verified'
-                })
-            } else {
-                res.send({
-                    status: 'FAILED',
-                    message: 'Invalid OTP'
-                })
-            }
-        }
-    } catch (err) {
-        console.log("Error occured in password update try catch block", err)
-        res.json({
-            status: 'FAILED',
-            message: 'Remote server unreachable'
-        })
-    }
-}
-
-exports.passwordChange = async (req, res) => {
-    const {
-        uuid,
-        password
-    } = req.body
-    try {
-        const updated = await User.update({ password }, {
-            where: { uuid }
-        })
-        if (updated) {
-            res.send({
-                status: 'SUCCESS',
-                message: 'Your password changed'
-            })
-        } else {
-            res.json({
-                status: 'FAILED',
-                message: 'You are not authenticated'
-            })
-        }
-    } catch (err) {
-        console.log("Error occured in password change try catch block", err)
-        res.json({
-            status: 'FAILED',
-            message: 'Remote server unreachable'
-        })
-    }
-}
