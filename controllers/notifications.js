157ea085f51c7cbeb94935a3b47f0705faeeee37
AbhiKrishnan <83958925+Abhi-akhila@users.noreply.github.com>
2022-03-09 23:06:54 +0530

    user added into notificatios

--- controllers/notifications.js
+++ controllers/notifications.js
@@ -1,35 +1,37 @@
-const { Notification } = require('../models')
-const { validate } = require('../commonFunctions/reqValidation')
+const { Notification, User } = require('../models')
 
 exports.notifications = async (req, res) => {
     const {
         title,
-        content
+        content,
+        uuid
     } = req.body
     try {
-        const isValid = validate(req.body)
-        if (isValid) {
-            await Notification.create({
-                title,
-                content
-            }).then(data => {
-                res.json({
-                    status: 'SUCCESS',
-                    message: 'Notification added'
-                })
-            }).catch(err => {
-                console.log("Error occured in notifications try catch block", err)
-                res.json({
-                    status: 'FAILED',
-                    message: 'Couldnt complete'
-                })
-            })
-        } else {
-            res.json({
-                status: 'FAILED',
-                message: 'Check your credentials!!!'
+        let user
+        if (uuid !== '' && uuid !== undefined) {
+            user = await User.findOne({
+                attributes: ['id'],
+                where: { uuid }
             })
         }
+        let userId = user ? user.id : null
+        await Notification.create({
+            title,
+            content,
+            userId
+        }).then(data => {
+            res.json({
+                status: 'SUCCESS',
+                message: 'Notification added',
+                data
+            })
+        }).catch(err => {
+            console.log("Error occured in notifications try catch block", err)
+            res.json({
+                status: 'FAILED',
+                message: 'Couldnt complete'
+            })
+        })
     } catch (err) {
         console.log("Error occured in notifications", err)
         res.json({
@@ -42,7 +44,8 @@ exports.notifications = async (req, res) => {
 exports.getNotifications = async (req, res) => {
     try {
         await Notification.findAll({
-            attributes: { exclude: ['id'] }
+            attributes: { exclude: ['id'] },
+            where: { userId: null }
         })
             .then(data => {
                 res.json({
@@ -58,6 +61,33 @@ exports.getNotifications = async (req, res) => {
             })
     } catch (err) {
         console.log("Error ocuured in notifications getNotifications try catch block", err)
+        res.json({
+            status: 'FAILED',
+            message: 'Remote server unreachable'
+        })
+    }
+}
+
+exports.getNotificationById = async (req, res) => {
+    const { uuid } = req.body
+    try {
+        const user = await User.findOne({
+            where: { uuid },
+            include: [{ model: Notification }]
+        })
+        if (user) {
+            res.json({
+                status: 'SUCCESS',
+                notifications: user.Notifications
+            })
+        } else {
+            res.json({
+                status: 'FAILED',
+                message: 'Internal server error'
+            })
+        }
+    } catch (error) {
+        console.log("Error occured in try catch block", err)
         res.json({
             status: 'FAILED',
             message: 'Remote server unreachable'
